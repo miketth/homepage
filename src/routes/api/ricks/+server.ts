6d3397a182eef2096f370be080939786be6cb36c
Miklós Tóth <tothmiklostibor@gmail.com>
2023-05-22 00:27:02 +0200

    Use async/await in Cloudflare KV store

--- src/routes/api/ricks/+server.ts
+++ src/routes/api/ricks/+server.ts
@@ -6,10 +6,12 @@ export const GET = (({ platform }) => {
   return json(ricks)
 }) satisfies RequestHandler
 
-export const POST = (({ platform }) => {
-  let ricks = 1
+export const POST = (async ({ platform }) => {
+  let ricks = 0
   if (platform?.env?.HOMEPAGE) {
-    ricks = ++platform.env.HOMEPAGE["ricks"] || 1
+    ricks = platform.env.HOMEPAGE.get("ricks", { type: "json" }) || 0
+    ricks++
+    await platform.env.HOMEPAGE.put("ricks", JSON.stringify(ricks))
   }
 
   return json(ricks)
